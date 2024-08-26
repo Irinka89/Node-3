980e186cf36b2b974a175f7df1065150b984bbd9
Irinka89 <irynalomonos@gmail.com>
2024-08-26 22:48:55 +0200

    comments added

--- task2.js
+++ task2.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+// Создайте файл с именем `info.txt` и запишите в него текст "Node.js is awesome!"
 fs.writeFile('info.txt', 'Node.js is awesome!', (err) => {
     if (err) {
         console.error('Ошибка при записи в файл:', err);
@@ -8,6 +9,7 @@ fs.writeFile('info.txt', 'Node.js is awesome!', (err) => {
     }
 });
 
+// Прочитайте содержимое файла `info.txt`.
 fs.readFile('info.txt', 'utf8', (err, data) => {
     if (err) {
         console.error('Ошибка при чтении файла:', err);
